e436111062ef0626e772a3ee7918aabcda259ddc
dylanmodugno <dylanmodugno@knights.ucf.edu>
2021-02-21 14:10:32 -0500

    Updated valid move function for rook

--- Power Chess/Assets/Scripts/Piece Scripts/Rook.cs
+++ Power Chess/Assets/Scripts/Piece Scripts/Rook.cs
@@ -6,7 +6,22 @@ public class Rook : Piece
 {
     public override bool ValidMove(int newX, int newZ)
     {
-        //TODO: Define movement for Rook
-        return true;
+        // If  newX and newZ are off the board
+        if (newX < 0 || newX > 7)
+            return false;
+        if (newZ < 0 || newZ > 7)
+            return false;
+        
+        // If rank stays the same
+        if (PositionX == newX)
+            return true;
+        
+        // If file stays the same
+        if (PositionZ == newZ)
+            return true;
+
+        // Otherwise it's an invalid move
+        else
+            return false;
     }
 }
