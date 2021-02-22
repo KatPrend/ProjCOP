25afd7ac16f9844a9f6efac50742fb98d6ac4429
verthoqn <veronica.qnguyen@gmail.com>
2021-02-21 19:03:32 -0500

    Updated bishop script

--- Power Chess/Assets/Scripts/Piece Scripts/Bishop.cs
+++ Power Chess/Assets/Scripts/Piece Scripts/Bishop.cs
@@ -6,7 +6,25 @@ public class Bishop : Piece
 {
     public override bool ValidMove(int newX, int newZ)
     {
-        //TODO: Define movement for Bishop
-        return true;
+        // Check for diagonal movement
+        int CompareX = 0, CompareZ = 0;
+
+        // Return false for out-of-bounds
+        if (newX < 0 || newX > 7)
+            return false;
+        if (newZ < 0 || newZ > 7)
+            return false;
+
+        // Find X comparison value
+        CompareX = PositionX - newX;
+
+        // Find Z comparison value
+        CompareZ = PositionZ - newZ;
+
+        // Check for valid diagonal movement
+        if ((CompareX == CompareZ) || (CompareX == -CompareZ))
+            return true;
+
+        return false;
     }
 }
